a9dacedc39ab9e2fcf543b11fa974536ec5018bc
Sai Surisetti <sai.surisetti@student.griffith.ie>
2024-10-04 20:36:57 +0100

    cleanup

--- WebApp/clinic-web-app/src/App.js
+++ WebApp/clinic-web-app/src/App.js
@@ -1,82 +1,178 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import Select from 'react-select';
 import axios from 'axios';
 
 function App() {
-  const [symptoms, setSymptoms] = useState([]);
   const [selectedSymptoms, setSelectedSymptoms] = useState([]);
   const [predictions, setPredictions] = useState('');
 
-  useEffect(() => {
-    // Fetch the list of symptoms dynamically from the Flask API if available
-    // Alternatively, define them statically if your API does not support it
-    setSymptoms([
-      "itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing", "shivering", "chills", "joint_pain",
-      "stomach_pain", "acidity", "ulcers_on_tongue", "muscle_wasting", "vomiting", "burning_micturition",
-      "spotting_urination", "fatigue", "weight_gain", "anxiety", "cold_hands_and_feets", "mood_swings",
-      "weight_loss", "restlessness", "lethargy", "patches_in_throat", "irregular_sugar_level", "cough",
-      "high_fever", "sunken_eyes", "breathlessness", "sweating", "dehydration", "indigestion", "headache",
-      "yellowish_skin", "dark_urine", "nausea", "loss_of_appetite", "pain_behind_the_eyes", "back_pain",
-      "constipation", "abdominal_pain", "diarrhoea", "mild_fever", "yellow_urine", "yellowing_of_eyes",
-      "acute_liver_failure", "fluid_overload_1", "swelling_of_stomach", "swelled_lymph_nodes", "malaise",
-      "blurred_and_distorted_vision", "phlegm", "throat_irritation", "redness_of_eyes", "sinus_pressure",
-      "runny_nose", "congestion", "chest_pain", "weakness_in_limbs", "fast_heart_rate", "pain_during_bowel_movements",
-      "pain_in_anal_region", "bloody_stool", "irritation_in_anus", "neck_pain", "dizziness", "cramps", "bruising",
-      "obesity", "swollen_legs", "swollen_blood_vessels", "puffy_face_and_eyes", "enlarged_thyroid", "brittle_nails",
-      "swollen_extremities", "excessive_hunger", "extra_marital_contacts", "drying_and_tingling_lips", "slurred_speech",
-      "knee_pain", "hip_joint_pain", "muscle_weakness", "stiff_neck", "swelling_joints", "movement_stiffness",
-      "spinning_movements", "loss_of_balance", "unsteadiness", "weakness_of_one_body_side", "loss_of_smell",
-      "bladder_discomfort", "foul_smell_of_urine", "continuous_feel_of_urine", "passage_of_gases", "internal_itching",
-      "toxic_look_(typhos)", "depression", "irritability", "muscle_pain", "altered_sensorium", "red_spots_over_body",
-      "belly_pain", "abnormal_menstruation", "dischromic_patches", "watering_from_eyes", "increased_appetite", "polyuria",
-      "family_history", "mucoid_sputum", "rusty_sputum", "lack_of_concentration", "visual_disturbances",
-      "receiving_blood_transfusion", "receiving_unsterile_injections", "coma", "stomach_bleeding", "distention_of_abdomen",
-      "history_of_alcohol_consumption", "fluid_overload_2", "blood_in_sputum", "prominent_veins_on_calf", "palpitations",
-      "painful_walking", "pus_filled_pimples", "blackheads", "scurring", "skin_peeling", "silver_like_dusting",
-      "small_dents_in_nails", "inflammatory_nails", "blister", "red_sore_around_nose", "yellow_crust_ooze"
-    ]);
-  }, []);
+  const symptomsOptions = [
+    { label: "Itching", value: "itching" },
+    { label: "Skin Rash", value: "skin_rash" },
+    { label: "Nodal Skin Eruptions", value: "nodal_skin_eruptions" },
+    { label: "Continuous Sneezing", value: "continuous_sneezing" },
+    { label: "Shivering", value: "shivering" },
+    { label: "Chills", value: "chills" },
+    { label: "Joint Pain", value: "joint_pain" },
+    { label: "Stomach Pain", value: "stomach_pain" },
+    { label: "Acidity", value: "acidity" },
+    { label: "Ulcers on Tongue", value: "ulcers_on_tongue" },
+    { label: "Muscle Wasting", value: "muscle_wasting" },
+    { label: "Vomiting", value: "vomiting" },
+    { label: "Burning Micturition", value: "burning_micturition" },
+    { label: "Spotting Urination", value: "spotting_urination" },
+    { label: "Fatigue", value: "fatigue" },
+    { label: "Weight Gain", value: "weight_gain" },
+    { label: "Anxiety", value: "anxiety" },
+    { label: "Cold Hands and Feets", value: "cold_hands_and_feets" },
+    { label: "Mood Swings", value: "mood_swings" },
+    { label: "Weight Loss", value: "weight_loss" },
+    { label: "Restlessness", value: "restlessness" },
+    { label: "Lethargy", value: "lethargy" },
+    { label: "Patches in Throat", value: "patches_in_throat" },
+    { label: "Irregular Sugar Level", value: "irregular_sugar_level" },
+    { label: "Cough", value: "cough" },
+    { label: "High Fever", value: "high_fever" },
+    { label: "Sunken Eyes", value: "sunken_eyes" },
+    { label: "Breathlessness", value: "breathlessness" },
+    { label: "Sweating", value: "sweating" },
+    { label: "Dehydration", value: "dehydration" },
+    { label: "Indigestion", value: "indigestion" },
+    { label: "Headache", value: "headache" },
+    { label: "Yellowish Skin", value: "yellowish_skin" },
+    { label: "Dark Urine", value: "dark_urine" },
+    { label: "Nausea", value: "nausea" },
+    { label: "Loss of Appetite", value: "loss_of_appetite" },
+    { label: "Pain Behind the Eyes", value: "pain_behind_the_eyes" },
+    { label: "Back Pain", value: "back_pain" },
+    { label: "Constipation", value: "constipation" },
+    { label: "Abdominal Pain", value: "abdominal_pain" },
+    { label: "Diarrhoea", value: "diarrhoea" },
+    { label: "Mild Fever", value: "mild_fever" },
+    { label: "Yellow Urine", value: "yellow_urine" },
+    { label: "Yellowing of Eyes", value: "yellowing_of_eyes" },
+    { label: "Acute Liver Failure", value: "acute_liver_failure" },
+    { label: "Fluid Overload 1", value: "fluid_overload_1" },
+    { label: "Swelling of Stomach", value: "swelling_of_stomach" },
+    { label: "Swelled Lymph Nodes", value: "swelled_lymph_nodes" },
+    { label: "Malaise", value: "malaise" },
+    { label: "Blurred and Distorted Vision", value: "blurred_and_distorted_vision" },
+    { label: "Phlegm", value: "phlegm" },
+    { label: "Throat Irritation", value: "throat_irritation" },
+    { label: "Redness of Eyes", value: "redness_of_eyes" },
+    { label: "Sinus Pressure", value: "sinus_pressure" },
+    { label: "Runny Nose", value: "runny_nose" },
+    { label: "Congestion", value: "congestion" },
+    { label: "Chest Pain", value: "chest_pain" },
+    { label: "Weakness in Limbs", value: "weakness_in_limbs" },
+    { label: "Fast Heart Rate", value: "fast_heart_rate" },
+    { label: "Pain During Bowel Movements", value: "pain_during_bowel_movements" },
+    { label: "Pain in Anal Region", value: "pain_in_anal_region" },
+    { label: "Bloody Stool", value: "bloody_stool" },
+    { label: "Irritation in Anus", value: "irritation_in_anus" },
+    { label: "Neck Pain", value: "neck_pain" },
+    { label: "Dizziness", value: "dizziness" },
+    { label: "Cramps", value: "cramps" },
+    { label: "Bruising", value: "bruising" },
+    { label: "Obesity", value: "obesity" },
+    { label: "Swollen Legs", value: "swollen_legs" },
+    { label: "Swollen Blood Vessels", value: "swollen_blood_vessels" },
+    { label: "Puffy Face and Eyes", value: "puffy_face_and_eyes" },
+    { label: "Enlarged Thyroid", value: "enlarged_thyroid" },
+    { label: "Brittle Nails", value: "brittle_nails" },
+    { label: "Swollen Extremities", value: "swollen_extremities" },
+    { label: "Excessive Hunger", value: "excessive_hunger" },
+    { label: "Extra Marital Contacts", value: "extra_marital_contacts" },
+    { label: "Drying and Tingling Lips", value: "drying_and_tingling_lips" },
+    { label: "Slurred Speech", value: "slurred_speech" },
+    { label: "Knee Pain", value: "knee_pain" },
+    { label: "Hip Joint Pain", value: "hip_joint_pain" },
+    { label: "Muscle Weakness", value: "muscle_weakness" },
+    { label: "Stiff Neck", value: "stiff_neck" },
+    { label: "Swelling Joints", value: "swelling_joints" },
+    { label: "Movement Stiffness", value: "movement_stiffness" },
+    { label: "Spinning Movements", value: "spinning_movements" },
+    { label: "Loss of Balance", value: "loss_of_balance" },
+    { label: "Unsteadiness", value: "unsteadiness" },
+    { label: "Weakness of One Body Side", value: "weakness_of_one_body_side" },
+    { label: "Loss of Smell", value: "loss_of_smell" },
+    { label: "Bladder Discomfort", value: "bladder_discomfort" },
+    { label: "Foul Smell of Urine", value: "foul_smell_of_urine" },
+    { label: "Continuous Feel of Urine", value: "continuous_feel_of_urine" },
+    { label: "Passage of Gases", value: "passage_of_gases" },
+    { label: "Internal Itching", value: "internal_itching" },
+    { label: "Toxic Look (Typhos)", value: "toxic_look_(typhos)" },
+    { label: "Depression", value: "depression" },
+    { label: "Irritability", value: "irritability" },
+    { label: "Muscle Pain", value: "muscle_pain" },
+    { label: "Altered Sensorium", value: "altered_sensorium" },
+    { label: "Red Spots Over Body", value: "red_spots_over_body" },
+    { label: "Belly Pain", value: "belly_pain" },
+    { label: "Abnormal Menstruation", value: "abnormal_menstruation" },
+    { label: "Dischromic Patches", value: "dischromic_patches" },
+    { label: "Watering from Eyes", value: "watering_from_eyes" },
+    { label: "Increased Appetite", value: "increased_appetite" },
+    { label: "Polyuria", value: "polyuria" },
+    { label: "Family History", value: "family_history" },
+    { label: "Mucoid Sputum", value: "mucoid_sputum" },
+    { label: "Rusty Sputum", value: "rusty_sputum" },
+    { label: "Lack of Concentration", value: "lack_of_concentration" },
+    { label: "Visual Disturbances", value: "visual_disturbances" },
+    { label: "Receiving Blood Transfusion", value: "receiving_blood_transfusion" },
+    { label: "Receiving Unsterile Injections", value: "receiving_unsterile_injections" },
+    { label: "Coma", value: "coma" },
+    { label: "Stomach Bleeding", value: "stomach_bleeding" },
+    { label: "Distention of Abdomen", value: "distention_of_abdomen" },
+    { label: "History of Alcohol Consumption", value: "history_of_alcohol_consumption" },
+    { label: "Fluid Overload 2", value: "fluid_overload_2" },
+    { label: "Blood in Sputum", value: "blood_in_sputum" },
+    { label: "Prominent Veins on Calf", value: "prominent_veins_on_calf" },
+    { label: "Palpitations", value: "palpitations" },
+    { label: "Painful Walking", value: "painful_walking" },
+    { label: "Pus Filled Pimples", value: "pus_filled_pimples" },
+    { label: "Blackheads", value: "blackheads" },
+    { label: "Scurring", value: "scurring" },
+    { label: "Skin Peeling", value: "skin_peeling" },
+    { label: "Silver Like Dusting", value: "silver_like_dusting" },
+    { label: "Small Dents in Nails", value: "small_dents_in_nails" },
+    { label: "Inflammatory Nails", value: "inflammatory_nails" },
+    { label: "Blister", value: "blister" },
+    { label: "Red Sore Around Nose", value: "red_sore_around_nose" },
+    { label: "Yellow Crust Ooze", value: "yellow_crust_ooze" }
+  ];
+  
 
-  const handleSymptomChange = (event) => {
-    const { value, checked } = event.target;
-    setSelectedSymptoms(prev => (
-      checked ? [...prev, value] : prev.filter(symptom => symptom !== value)
-    ));
+  const handleSymptomChange = selectedOptions => {
+    setSelectedSymptoms(selectedOptions);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const queryString = selectedSymptoms.map(symptom => `symptom=${encodeURIComponent(symptom)}`).join('&');
-      const response = await axios.get(`https://saisuri2015.pythonanywhere.com/predict?${queryString}`);
-      setPredictions(response.data);
-    } catch (error) {
-      console.error('Error fetching predictions:', error);
-    }
+  const handleSubmit = async () => {
+    const symptoms = selectedSymptoms.map(symptom => symptom.value);
+    const response = await axios.get(`https://saisuri2015.pythonanywhere.com/predict?symptom=${symptoms.join('&symptom=')}`);
+    setPredictions(response.data);
   };
 
   return (
     <div className="App">
-      <h1>Symptom Checker</h1>
-      <form onSubmit={handleSubmit}>
-        {symptoms.map(symptom => (
-          <div key={symptom}>
-            <input
-              type="checkbox"
-              id={symptom}
-              value={symptom}
-              onChange={handleSymptomChange}
-              checked={selectedSymptoms.includes(symptom)}
-            />
-            <label htmlFor={symptom}>{symptom}</label>
+      <div className="content">
+        <h1>Symptom Checker</h1>
+        <Select
+          isMulti
+          options={symptomsOptions}
+          onChange={handleSymptomChange}
+          className="symptom-select"
+          classNamePrefix="select"
+          placeholder="Select symptoms..."
+        />
+        <button onClick={handleSubmit} className="submit-btn">Check Symptoms</button>
+        {predictions && (
+          <div className="predictions">
+            <h2>Predictions</h2>
+            <p>{predictions}</p>
           </div>
-        ))}
-        <button type="submit">Check Symptoms</button>
-      </form>
-      {predictions && <div>
-        <h2>Predictions</h2>
-        <p>{predictions}</p>
-      </div>}
+        )}
+      </div>
     </div>
   );
 }
